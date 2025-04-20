8c2126705e460e3d2a555f5547913579f8e5aedb
Safiro <altxafiro@gmail.com>
2025-04-20 19:53:19 +0300

    feat(ui): Add initial hero styles

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,40 @@
-'use strict';
+"use strict";
+
+// Header
+
+const headerNode = document.querySelector(".header");
+const headerInnerNode = document.querySelector(".header__inner");
+const heroInnerNode = document.querySelector(".hero__inner");
+
+const HeaderStyles = {
+  BACKGROUND_COLOR: "transparent",
+  BACKGROUND_COLOR_SCROLLED: "var(--color-black-700)",
+  PADDING_RIGHT: "var(--header-padding-right)",
+  PADDING_RIGHT_SCROLLED: "0",
+};
+
+const setInitialHeaderStyles = () => {
+  headerNode.style.backgroundColor = HeaderStyles.BACKGROUND_COLOR;
+  headerInnerNode.style.paddingRight = HeaderStyles.PADDING_RIGHT;
+};
+
+const setScrolledHeaderStyles = () => {
+  headerNode.style.backgroundColor = HeaderStyles.BACKGROUND_COLOR_SCROLLED;
+  headerInnerNode.style.paddingRight = HeaderStyles.PADDING_RIGHT_SCROLLED;
+};
+
+const setHeaderStyles = () => {
+  if (window.scrollY === 0) {
+    setInitialHeaderStyles();
+  } else {
+    setScrolledHeaderStyles();
+  }
+};
+
+const handleWindowScroll = () => {
+  setHeaderStyles();
+};
+
+setHeaderStyles();
+
+window.addEventListener("scroll", handleWindowScroll);
